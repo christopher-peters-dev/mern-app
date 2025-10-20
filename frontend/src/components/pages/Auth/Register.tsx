4a794f2ac02ef4e49a1d9fd9cb9c25b83f51ec66
christopher peters <christopher.peters.dev@gmail.com>
2025-10-20 21:55:21 +0530

    updated form components to shadcn

--- frontend/src/components/pages/Auth/Register.tsx
+++ frontend/src/components/pages/Auth/Register.tsx
@@ -1,16 +1,12 @@
-import { useForm, type SubmitHandler } from "react-hook-form";
-import InputField from "@/components/common/Form/InputField";
-import ActionButton from "@/components/common/Interactive/ActionButton";
-import { Link } from "react-router";
+import { useForm, Controller, type SubmitHandler } from "react-hook-form";
+import { Input } from "@/components/ui/input";
+import { Field, FieldError, FieldLabel } from "@/components/ui/field";
 import { registerSchema, type RegisterInput } from "@/schemas/register.schema";
 import { zodResolver } from "@hookform/resolvers/zod";
+import ActionButton from "@/components/common/Interactive/ActionButton";
 
 const Register = () => {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterInput>({
+  const form = useForm<RegisterInput>({
     defaultValues: {
       firstName: "",
       lastName: "",
@@ -27,55 +23,107 @@ const Register = () => {
     <div className="container mx-auto flex flex-col flex-1 py-10">
       <div className="flex flex-col">
         <span className="text-4xl font-bold mb-10">Create an Account</span>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex gap-10 justify-between">
-            <InputField
-              fieldType="text"
-              fieldTitle="First Name"
-              {...register("firstName")}
-              errors={errors}
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <div className="flex gap-10">
+            <Controller
+              name="firstName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="firstName">First Name</FieldLabel>
+                  <Input
+                    {...field}
+                    type="text"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="enter first name"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
 
-            <InputField
-              fieldType="text"
-              fieldTitle="Last Name"
-              {...register("lastName")}
-              errors={errors}
+            <Controller
+              name="lastName"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="lastName">Last Name</FieldLabel>
+                  <Input
+                    {...field}
+                    type="text"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="enter last name"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
           </div>
-          <div className="flex">
-            <InputField
-              fieldType="email"
-              fieldTitle="Email"
-              {...register("email")}
-              errors={errors}
+          <div className="flex flex-col mt-10">
+            <Controller
+              name="email"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="email">Email</FieldLabel>
+                  <Input
+                    {...field}
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="enter email"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
           </div>
-          <div className="flex">
-            <InputField
-              fieldType="password"
-              fieldTitle="Password"
-              {...register("password")}
-              errors={errors}
+          <div className="flex gap-10 mt-10 mb-10">
+            <Controller
+              name="password"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="password">Password</FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="enter password"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
+            />
+            <Controller
+              name="confirmPassword"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field data-invalid={fieldState.invalid}>
+                  <FieldLabel htmlFor="confirmPassword">
+                    Confirm Password
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    type="password"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="enter password"
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
           </div>
-          <div className="flex">
-            <InputField
-              fieldType="password"
-              fieldTitle="Confirm Password"
-              {...register("confirmPassword")}
-              errors={errors}
-            />
-          </div>
-          <div className="flex justify-between mt-5">
-            <p>
-              Already Registered?{" "}
-              <Link to="/signin" className="underline">
-                Sign in here
-              </Link>
-            </p>
-            <ActionButton name="createAccount" />
-          </div>
+          <ActionButton name="createAccount" />
         </form>
       </div>
     </div>
